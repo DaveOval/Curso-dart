6bbe9f1b92ed3a381128d238fa449ec2b2e4a3b2
Carlos David <dave_u@outlook.com>
2023-01-25 17:43:52 -0600

    se agren mas temas

---
+++ 06-clases/01-clases.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  // final persona = {
+  //   'nombre': 'Fernando',
+  //   'edad': 33
+  // };
+
+  // print(persona);
+}
+
+class Persona{
+  //Campos o propiedads
+  String? nombre;
+  //Get y sets
+
+  //Constructores
+
+  //Metodos (funciones)
+
+
+}
